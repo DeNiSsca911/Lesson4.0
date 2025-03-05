d69f025f725c2d03cd899e421cff4f2af624d483
DeNiSsca911 <denwertyu@gmail.com>
2025-03-05 18:48:50 +0300

    Revert "delete 5/5"
    This reverts commit bdd50c456ae063e38f29799572a0739d1f96fe1d.

--- mane.py
+++ mane.py
@@ -1,9 +1,16 @@
 
+print(" КОНФЛИКТ")
 
+for i in range(8)
+    print("RUN")
+a,b = 9,8
 
-
-
-
+if a > b :
+    print ( " a больше чем  b")
+elif b > a :
+    print("b больше чем  а")
+else:
+    print("a равно b ")
 print("Hello World")
 
 print("Hello PyCharm")
